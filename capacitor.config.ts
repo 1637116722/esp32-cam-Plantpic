994d70cb1550d9b631363c138a518e395b7a63af
Jimmy <Wang20050630@gmail.com>
2026-02-04 15:43:23 +0800

    ios webview allow navigation and ATS

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,10 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.plantalk.app',
   appName: 'PlanTalk',
-  webDir: 'dist'
+  webDir: 'dist',
+  server: {
+    allowNavigation: ['plantalk-app.vercel.app']
+  }
 };
 
 export default config;
